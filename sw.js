80cca1741c6fd5dd2fc7e861f9f381b44fbbc096
haoxuweiJuly <haoxu.wei@acsinternational.edu.sg>
2026-09-17 13:19:35 +0800

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,32 @@
+/* 柱壳法 · 离线缓存。页面更新后，把下面的版本号 +1，同学的浏览器才会拉新版。 */
+const CACHE = "shell-method-v1";
+const ASSETS = ["./", "./index.html", "./manifest.json",
+                "./icon-192.png", "./icon-512.png"];
+
+self.addEventListener("install", e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting()));
+});
+
+self.addEventListener("activate", e => {
+  e.waitUntil(
+    caches.keys()
+      .then(ks => Promise.all(ks.filter(k => k !== CACHE).map(k => caches.delete(k))))
+      .then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener("fetch", e => {
+  if (e.request.method !== "GET") return;
+  e.respondWith(
+    caches.match(e.request, { ignoreSearch: true }).then(hit => {
+      if (hit) return hit;
+      return fetch(e.request).then(res => {
+        if (res && res.status === 200 && res.type === "basic") {
+          const copy = res.clone();
+          caches.open(CACHE).then(c => c.put(e.request, copy));
+        }
+        return res;
+      }).catch(() => caches.match("./index.html"));
+    })
+  );
+});
